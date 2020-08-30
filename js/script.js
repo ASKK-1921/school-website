f0c5ffbf67ab29187653de2932d3f4b82c9b669d
Andrew Kellock <andrew.kellock@laurus-school.com>
2020-08-30 16:12:12 +0900

    Updated Javscript to use modular style.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,57 @@
 // JavaScript Document
 
-// Slideshow
-var slideIndex = 0;
-showSlides();
+// UI Controler
+var userInterfaceController = (function(){
+	
+	var DOMstrings = {
+		slidesImage: ('.slides_image'),
+		slidesDot: ('.slides_dot')
+	}
+	
+	return {
+		slideShow: function(){
+			// Slideshow
+			var slideIndex = 0;
+			showSlides();
 
-function showSlides() {
-	var i;
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot");
-	
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+			function showSlides() {
+				var i;
+				var slides = document.querySelectorAll(DOMstrings.slidesImage);
+				var dots = document.querySelectorAll(DOMstrings.slidesDot);
+
+				for (i = 0; i < slides.length; i++) {
+					slides[i].style.display = "none";
+				}
+
+				slideIndex++;
+
+				if (slideIndex > slides.length) {slideIndex = 1}
+
+				for (i = 0; i < dots.length; i++) {
+					dots[i].className = dots[i].className.replace(" active", "");
+				}
+
+				slides[slideIndex-1].style.display = "block";
+				dots[slideIndex-1].className += " active";
+				setTimeout(showSlides, 5000);
+			}
+		},
+		
+		getDOMstrings: function() {
+			return DOMstrings;
+		}
 	}
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(UIctrl){
 	
-	slideIndex++;
-	
-	if (slideIndex > slides.length) {slideIndex = 1}
-	
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
+	return {
+		init: function() {
+			console.log('Application has started.');
+			UIctrl.slideShow();
+		}
 	}
-	
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " active";
-	setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
+})(userInterfaceController);
+
+controller.init();
